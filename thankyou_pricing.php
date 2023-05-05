e64df8187d17a0989d28dc2b0e30198d71898f8d
regita-99 <regitarose9@gmail.com>
2023-05-05 10:25:26 +0700

    update

---
+++ thankyou_pricing.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Template Name: Thankyou
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<section class="value-siakad" style="margin-top:0rem; margin-bottom:0rem; padding-top:2rem; ">
+    <div class="container">
+        <div class="row value logo-siakad">
+            <div class="col-12 col-lg-8 text-center m-auto">
+            </div>
+            <div class="col-12">
+                <div class="grid-value">
+                    <div class="item">
+                        <!-- <div class="inner" style="padding:0rem; align-items:;">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/thankyou.png" class="img-value img-fluid" alt="" style="padding-right:15px;">
+								<div class="text-body">
+									<h4 class="heading" style="font-weight:bold; font-size: 32px;">Terima Kasih Telah Mengisi Form!</h4>
+									<p class="desc" style="font-size: 24px; line-height:150%;">
+                                    Selangkah lagi sistem informasi akademik akan memudahkan pengelolaan akademik Anda.<br><br>
+                                    Butuh konsultasi cepat dan mendapatkan promo siAkadCloud? Silahkan berkonsultasi dengan
+                                    Konsultan Executive SEVIMA melalui Whatsapp dibawah ini.
+									</p>
+								</div>								
+							</div> -->
+                    </div>
+
+                    <div class="item">
+                        <div class="inner">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/thankyou.png" class="img-value img-fluid" alt="">
+                            <div class="text-body">
+                                <h4 class="heading" style="font-weight:bold; font-size: 32px;">Terima Kasih Telah Mengisi Form!</h4>
+                                <p class="desc" style="font-size: 24px; line-height:150%;">
+                                    Selangkah lagi sistem informasi akademik akan memudahkan pengelolaan akademik Anda.<br><br>
+                                    Butuh konsultasi cepat dan dapatkan promo siAkadCloud? Silahkan berkonsultasi dengan
+                                    Konsultan Executive SEVIMA melalui Whatsapp di bawah ini.
+                                </p>
+                                <div class="col-12">
+                                    <div class="button-selengkapnya" style="justify-content:normal;">
+                                        <a class="btn btn-primary" href="https://wa.me/6282261610404?text=[SC3204]%20Halo%20Saya%20ingin%20bertanya%20tentang%20layanan%20SEVIMA%20siAkad%20Cloud" style="margin-left:-15px;">Hubungi SEVIMA</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<!-- 
+<section class="value-siakad" style="margin-top:0rem; margin-bottom:0rem;">
+		<div class="container">
+			<div class="row value logo-siakad">	
+            <div class="col-12">									
+						<div class="item">
+							<div class="inner">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/thankyou.png" class="img-value img-fluid" alt="">
+								<div class="text-body">
+									<h4 class="heading">Terima Kasih Telah Mengisi Form!</h4>
+									<p class="desc">
+										Selangkah lagi sistem informasi akadmeik akan<br>
+                                        memudahkan pengelolaan akademik Anda. Butuh konsultasi<br>
+                                        cepat dan mendapatkan promo siAkadCloud dapat<br>
+                                        berkonsultasi dengan Konsultan Executive SEVIMA<br>
+                                        melalui Whatsapp dibawah ini.
+									</p>
+								</div>								
+										
+					
+
+                <a class="btn btn-sevima ml-auto" href="https://wa.me/6282261610404?text=[SC3204]%20Halo%20Saya%20ingin%20bertanya%20tentang%20layanan%20SEVIMA%20siAkad%20Cloud" role="button"><i
+            class="uil uil-whatsapp"></i>Hubungi SEVIMA</a>
+			</div>
+            </div>
+						</div>	
+		</div>
+        </div>
+	</section> -->
+
+
+<?php get_footer(); ?>
